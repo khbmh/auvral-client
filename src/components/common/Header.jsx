5f99a9c2e87d10020faf7ebcef430dcc7ca62166
khbmh (Mahi) <khaledbinmonowarhossain@gmail.com>
2024-12-24 05:48:54 +0600

    navlinks set

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -4,14 +4,35 @@ import { AuthContext } from '../contexts/AuthContext';
 
 function Header() {
   const { user, logOut, userLoading } = useContext(AuthContext);
-
+  // console.log(user.photoURL);
+  // console.log(user);
+  const [isHovered, setIsHovered] = useState(false);
   const menu = (
     <ul className="flex flex-col lg:flex-row gap-3 lg:gap-6">
       <NavLink to="/">Home</NavLink>
       <NavLink to="/all-artifacts">All artifacts</NavLink>
       <NavLink to="/add-artifacts">Add artifacts</NavLink>
-      <NavLink to="/my-added-artifacts">My Added artifacts</NavLink>
-      <NavLink to="/my-loved-artifacts">My Loved Profile</NavLink>
+      <div
+        className="w-fit relative"
+        onMouseEnter={() => setIsHovered(true)} // when mouse enters, set isHovered to true
+        onMouseLeave={() => setIsHovered(false)} // when mouse leaves, set isHovered to false
+      >
+        <p className="cursor-pointer">My Profile</p>
+        <ul
+          className={`lg:absolute w-[170px] ${
+            isHovered ? 'lg:flex' : 'lg:hidden'
+          } flex-col py-2`}
+        >
+          <li>
+            <NavLink to="/my-loved-artifacts">My Loved Profile</NavLink>
+          </li>
+          <li>
+            <NavLink to="/my-added-artifacts">My Added artifacts</NavLink>
+          </li>
+        </ul>
+      </div>
+      {/* <NavLink to="/my-added-artifacts">My Added artifacts</NavLink> */}
+      {/* <NavLink to="/my-loved-artifacts">My Loved Profile</NavLink> */}
     </ul>
   );
 
@@ -58,13 +79,14 @@ function Header() {
               {menu}
             </ul>
           </div>
-          {/* <a className="btn btn-ghost text-xl">daisyUI</a> */}
-          <img
-            src="https://cdn3d.iconscout.com/3d/premium/thumb/medieval-knight-shield-3d-icon-download-in-png-blend-fbx-gltf-file-formats--armor-weapon-pack-sports-games-icons-9555744.png?f=webp"
-            alt="logo"
-            className="w-10"
-          />{' '}
-          <span className="text-xl ml-2 font-bold font-mono">Auvral</span>
+          <Link to="/" className="flex items-center">
+            <img
+              src="https://cdn3d.iconscout.com/3d/premium/thumb/medieval-knight-shield-3d-icon-download-in-png-blend-fbx-gltf-file-formats--armor-weapon-pack-sports-games-icons-9555744.png?f=webp"
+              alt="logo"
+              className="w-10"
+            />{' '}
+            <span className="text-xl ml-2 font-bold font-mono">Auvral</span>
+          </Link>
         </div>
         <div className="navbar-center hidden lg:flex">
           <ul className="menu menu-horizontal px-1">{menu}</ul>
@@ -86,7 +108,10 @@ function Header() {
                 <Link to="/login" className="btn mid btn-success">
                   Login
                 </Link>
-                <Link to="/register" className="btn btn-outline btn-success">
+                <Link
+                  to="/register"
+                  className="hidden btn btn-outline btn-success"
+                >
                   Register
                 </Link>
               </div>
@@ -96,12 +121,21 @@ function Header() {
                   userLoading && 'invisible'
                 } items-center space-x-3 flex ml-2`}
               >
-                <p className="mx-2 font-semibold text-md lg:text-xl flex">
-                  👋 {user.displayName}
-                </p>
-                <p onClick={logOut} className="btn mid">
-                  LogOut
-                </p>
+                <div className="relative mx-3 px-3">
+                  <img
+                    src={user.photoURL}
+                    alt={user.displayName}
+                    className="w-10"
+                  />
+                  <div className="absolute w-[300px] -ml-[60px] py-2">
+                    <p className="mx-2 font-semibold text-md lg:text-xl flex">
+                      {user.displayName}
+                    </p>
+                    <p onClick={logOut} className="btn mid">
+                      LogOut
+                    </p>
+                  </div>
+                </div>
               </div>
             )}
           </div>
